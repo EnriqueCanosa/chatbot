7c7df1824011efbd9b667f5c063d29e1a787bde4
EnriqueCanosa <137073157+EnriqueCanosa@users.noreply.github.com>
2024-09-04 09:58:10 -0300

    Add files via upload

---
+++ window.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const audio = document.getElementById('background-music');
+    audio.volume = 0.1; // Define o volume baixo para não ser invasivo
+})
